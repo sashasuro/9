149a6108c2e97e9c3a97260931a8b4d8977f92ac
sashasuro <123554714+sashasuro@users.noreply.github.com>
2024-03-05 10:09:30 +0300

    Add files via upload

---
+++ Enrollee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20
+{
+    class Enrollee
+    {
+        string name = "Oleg";
+        string surname = "Voronov";
+        DateTime bd = new DateTime(9, 5, 1998);
+        string faculty = "Arts";
+    }
+    class Student
+    {
+        string name = "Alexander";
+        string surname = "Kurp";
+        DateTime bd = new DateTime(6, 12, 2006);
+        string faculty = "Historical";
+        int kurs = 3;
+    }
+    class Teacher
+    {
+        string name = "Vlad";
+        string surname = "Norikov";
+        DateTime bd = new DateTime(21, 7, 1991);
+        string faculty = "Economic";
+        string jt = "Teacher";
+        int experience = 3;
+    }
+}
---
+++ Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20
+{
+    abstract class Person
+    {
+        string name;
+        string surname;
+        DateTime bd = new DateTime(2, 2, 2000);
+        public Person()
+        {
+            DateTime now = DateTime.Now;
+            TimeSpan dt = now - bd;
+        }
+    }
+}
